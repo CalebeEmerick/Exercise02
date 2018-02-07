064dbb94a2b145787db754f38c30848a5a80a1d9
Calebe Emerick <cemerik@stone.com.br>
2018-02-07 11:11:57 -0200

    Cria a estrutura do projeto.

---
+++ Source/Exercise02/Supporting FIles/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Exercise02
+//
+//  Created by Calebe Emerick on 07/02/2018.
+//  Copyright © 2018 Stone Pagamentos. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		
+		return true
+	}
+}
