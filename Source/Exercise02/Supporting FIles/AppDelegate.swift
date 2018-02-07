6ccbca59d58de6ebd458ea7940e74c0401374482
Calebe Emerick <cemerik@stone.com.br>
2018-02-07 18:58:36 -0200

    Cria a tela de clientes.

--- Source/Exercise02/Supporting FIles/AppDelegate.swift
+++ Source/Exercise02/Supporting FIles/AppDelegate.swift
@@ -15,6 +15,16 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		
+		let frame = UIScreen.main.bounds
+		let window = UIWindow(frame: frame)
+		
+		self.window = window
+		
+		let rootController = HomeController(title: "Clientes")
+		
+		window.rootViewController = rootController
+		window.makeKeyAndVisible()
+		
 		return true
 	}
 }
