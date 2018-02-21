b7e5be2dba24b22a6d2867cf77d78e8713d07db5
Calebe Emerick <cemerik@stone.com.br>
2018-02-21 17:48:31 -0300

    Cria o validador de data e seus testes.

---
+++ Source/Exercise02/Features/Registration/Validator/CompanyDateFormatter.swift
@@ -0,0 +1,50 @@
+//
+//  CompanyDateFormatter.swift
+//  Exercise02
+//
+//  Created by Calebe Emerick on 21/02/2018.
+//  Copyright © 2018 Stone Pagamentos. All rights reserved.
+//
+
+import Foundation
+
+final class CompanyDateFormatter: RegexBaseValidator {
+	
+	private let formatter: DateFormatter
+	private let pattern: String
+	
+	init(pattern: String, formatter: DateFormatter) {
+		self.pattern = pattern
+		self.formatter = formatter
+	}
+	
+	override init() {
+		pattern = "^[0-9]{2}/[0-9]{2}/[0-9]{4}$"
+		formatter = DateFormatter()
+		formatter.dateStyle = .short
+		formatter.locale = Locale(identifier: "pt_BR")
+	}
+	
+	func validate(date: String) -> Bool {
+		
+		let conditions: [Bool] = [
+			!date.isEmpty,
+			isDateInAValidFormat(date),
+			isDateReal(date)
+		]
+		
+		guard !conditions.contains(false) else {
+			return false
+		}
+		
+		return true
+	}
+	
+	private func isDateInAValidFormat(_ date: String) -> Bool {
+		return validate(date, with: pattern)
+	}
+	
+	private func isDateReal(_ date: String) -> Bool {
+		return formatter.date(from: date) != nil
+	}
+}
---
+++ Source/Exercise02Tests/Validators/CompanyDateFormatterTests.swift
@@ -0,0 +1,63 @@
+//
+//  CompanyDateFormatterTests.swift
+//  Exercise02Tests
+//
+//  Created by Calebe Emerick on 21/02/2018.
+//  Copyright © 2018 Stone Pagamentos. All rights reserved.
+//
+
+@testable import Exercise02
+import Nimble
+import XCTest
+
+final class CompanyDateFormatterTests: XCTestCase {
+	
+	private var validator: CompanyDateFormatter!
+	
+	override func setUp() {
+		super.setUp()
+		
+		validator = CompanyDateFormatter()
+	}
+	
+	override func tearDown() {
+		validator = nil
+		
+		super.tearDown()
+	}
+	
+	func test_shouldNotAccept_EmptyString() {
+		
+		let isEmpty = validator.validate(date: "")
+		
+		expect(isEmpty).to(beFalse())
+	}
+	
+	func test_shouldNotAccept_OnlyNumbers() {
+		
+		let onlyNumbers = validator.validate(date: "10102010")
+		
+		expect(onlyNumbers).to(beFalse())
+	}
+	
+	func test_shouldNotAccept_InvalidFormat() {
+		
+		let invalidFormat = validator.validate(date: "10-10-2010")
+		
+		expect(invalidFormat).to(beFalse())
+	}
+	
+	func test_shouldNotAccept_InvalidDate() {
+		
+		let invalidDate = validator.validate(date: "29/02/2005")
+		
+		expect(invalidDate).to(beFalse())
+	}
+	
+	func test_shouldAccept_ValidDate() {
+		
+		let validDate = validator.validate(date: "29/02/2004")
+		
+		expect(validDate).to(beTrue())
+	}
+}
